bce0c536d82e8b1af0a472d058870b6f4f6fd641
Ned Hulseman <ned.hulseman@gmail.com>
2024-11-18 13:54:53 -0500

    pausing mapping, working on formns

---
+++ views/sailingcommitteecommands.js
@@ -0,0 +1,20 @@
+﻿const express = require('express');
+const mysql = require("mysql")
+const dotenv = require('dotenv')
+
+const app = express();
+
+dotenv.config({ path: './.env' })
+
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+})
+
+index.hbs
+
+
+ALTER USER 'nhulseman'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dustPed15';
